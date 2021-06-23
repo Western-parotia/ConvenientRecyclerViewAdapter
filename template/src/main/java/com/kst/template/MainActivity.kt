2b9b0470a7a6e616ca7ca8c2fc54e5df77098fee
zhusw <sink50@foxmail.com>
2021-06-23 11:53:55 +0800

    dev:多类型示例

--- template/src/main/java/com/kst/template/MainActivity.kt
+++ template/src/main/java/com/kst/template/MainActivity.kt
@@ -3,10 +3,14 @@ package com.kst.template
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.foundation.widget.crvadapter.viewbinding.MultiViewBindingViewHolder
+import com.foundation.widget.crvadapter.viewbinding.ViewBindingMultiItemQuickAdapter
 import com.foundation.widget.crvadapter.viewbinding.ViewBindingQuickAdapter
 import com.foundation.widget.crvadapter.viewbinding.ViewBindingViewHolder
 import com.kst.template.databinding.ActivityMainBinding
-import com.kst.template.databinding.ItmeBookBinding
+import com.kst.template.databinding.ItemBookBinding
+import com.kst.template.databinding.ItemWeatherCloudyBinding
+import com.kst.template.databinding.ItemWeatherSunnyBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb.root)
         setBookData()
-
+        setWeathers()
 
     }
 
@@ -37,14 +41,61 @@ class MainActivity : AppCompatActivity() {
         }
         adapter.setNewData(data)
     }
+
+    private fun setWeathers() {
+        val adapter = WeatherListAdapter()
+        vb.rvWeather.let {
+            it.layoutManager = LinearLayoutManager(this)
+            it.adapter = adapter
+        }
+        val data = mutableListOf<WeatherBean>()
+        for (i in 0..20) {
+            val bean = if (i % 2 == 0) {
+                WeatherBean("晴天")
+            } else {
+                WeatherBean("阴天")
+            }
+            data.add(bean)
+        }
+        adapter.setNewData(data)
+    }
 }
 
-class BookListAdapter : ViewBindingQuickAdapter<ItmeBookBinding, BookBean>() {
+class BookListAdapter : ViewBindingQuickAdapter<ItemBookBinding, BookBean>() {
 
-    override fun convert(helper: ViewBindingViewHolder<ItmeBookBinding>, item: BookBean) {
+    override fun convert(helper: ViewBindingViewHolder<ItemBookBinding>, item: BookBean) {
         val vb = helper.viewBinding
         vb.tvName.text = item.name
-        vb.tvPrice.text = "¥${item.price}"
+        vb.ivPrice.text = "¥${item.price}"
+    }
+}
+
+class WeatherListAdapter : ViewBindingMultiItemQuickAdapter<WeatherBean>() {
+
+    init {
+        addItemType(1, ItemWeatherSunnyBinding::class.java)
+        addItemType(2, ItemWeatherCloudyBinding::class.java)
+    }
+
+    override fun convert(helper: MultiViewBindingViewHolder, item: WeatherBean) {
+        when (val itemType = item.itemType) {
+            1 -> {
+                val vb = helper.forceBinding<ItemWeatherSunnyBinding>(itemType)
+                convertSunny(vb, item)
+            }
+            2 -> {
+                val vb = helper.forceBinding<ItemWeatherCloudyBinding>(itemType)
+                convertCloudy(vb, item)
+            }
+        }
+    }
+
+    fun convertSunny(vb: ItemWeatherSunnyBinding, item: WeatherBean) {
+        vb.tvName.text = "type:1 ${item.weather}"
+    }
+
+    fun convertCloudy(vb: ItemWeatherCloudyBinding, item: WeatherBean) {
+        vb.tvName.text = "type:2 ${item.weather}"
     }
 
 }
--- template/src/main/java/com/kst/template/WeatherBean.kt
+++ template/src/main/java/com/kst/template/WeatherBean.kt
@@ -1,12 +1,17 @@
 package com.kst.template
 
 import com.chad.library.adapter.base.entity.MultiItemEntity
+import com.foundation.widget.crvadapter.viewbinding.ViewBindingMultiItemQuickAdapter
 
 /**
  * create by zhusw on 6/23/21 10:46
  */
-class WeatherBean(val temperature: Int, val state: Int) : MultiItemEntity {
+class WeatherBean(val weather: String) : MultiItemEntity {
 
-    override fun getItemType(): Int = state
+    override fun getItemType(): Int = when (weather) {
+        "晴天" -> 1
+        "阴天" -> 2
+        else -> ViewBindingMultiItemQuickAdapter.DEFAULT_VIEW_TYPE
+    }
 }
 
